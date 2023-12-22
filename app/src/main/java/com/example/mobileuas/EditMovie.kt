0744c9dbd6e2f6deefdd6a70bd7cbf4a91da7c3d
IlhamRafi73 <ilhamrafimahameru2004@mail.ugm.ac.id>
2023-12-23 01:26:05 +0700

    Initial Commit

--- app/src/main/java/com/example/mobileuas/EditMovie.kt
+++ app/src/main/java/com/example/mobileuas/EditMovie.kt
@@ -1,132 +1,119 @@
 package com.example.mobileuas
 
-import android.app.Activity
+
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
 import com.example.mobileuas.database.Movie
 import com.example.mobileuas.databinding.ActivityEditMovieBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import java.util.UUID
 
 class EditMovie : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditMovieBinding
     private var imageUri: Uri? = null
+    private var updateId: String? = null
     private lateinit var storageReference: StorageReference
+    private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            result.data?.data?.let {
+                imageUri = it
+                binding.imageView.setImageURI(it)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditMovieBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Get movie data from intent
-        val movie = intent.getSerializableExtra("movie") as Movie
-
-        // Populate UI with movie data
-        binding.apply {
-            Glide.with(this@EditMovie)
-                .load(movie.imageUrl)
-                .into(imageView)
-            editTextTitle.setText(movie.title)
-            editTextProducer.setText(movie.producer)
-            editTextRating.setText(movie.rating)
-            editTextDescription.setText(movie.description)
-        }
-
-        // Initialize Firebase Storage
         storageReference = FirebaseStorage.getInstance().reference
 
-
-        // Handle update button click
-        binding.btnUpdateMovie.setOnClickListener {
-            // Update the movie in Firebase
-            updateMovieInFirebase(movie)
-        }
-    }
-
-    // Constants
-    companion object {
-        private const val PICK_IMAGE_REQUEST = 1
-    }
-
-    // Handle the result of the image picker
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
-            // Get the selected image URI
-            imageUri = data.data
-            // Display the selected image
+        // Retrieve movie details from the intent
+        val movie: Movie? = intent.getParcelableExtra("movie")
+        if (movie != null) {
+            updateId = movie.id
             Glide.with(this)
-                .load(imageUri)
+                .load(movie.imageUrl)
                 .into(binding.imageView)
+            binding.editTextTitle.setText(movie.title)
+            binding.editTextProducer.setText(movie.producer)
+            binding.editTextRating.setText(movie.rating)
+            binding.editTextDescription.setText(movie.description)
+        }
+
+        binding.btnAddImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            getContent.launch(intent)
+        }
+
+        binding.btnUpdateMovie.setOnClickListener {
+            updateMovie()
         }
     }
 
-    // Update movie in Firebase with the new image
-    private fun updateMovieInFirebase(movie: Movie) {
-        // Upload the new image to Firebase Storage if it's changed
+    private fun updateMovie() {
         if (imageUri != null) {
-            uploadImageToFirebase(movie)
-        } else {
-            // If the image is not changed, update the movie without uploading the image
-            updateMovieDataInFirestore(movie, movie.imageUrl)
-        }
-    }
-
-    // Upload the new image to Firebase Storage
-    private fun uploadImageToFirebase(movie: Movie) {
-        val imageFileName = UUID.randomUUID().toString()
-        val imageRef = storageReference.child("images/$imageFileName")
-
-        imageRef.putFile(imageUri!!)
-            .addOnSuccessListener { taskSnapshot ->
-                // Image upload successful, get the download URL
-                imageRef.downloadUrl.addOnSuccessListener { uri ->
-                    // Update the movie data in Firestore with the new image URL
-                    updateMovieDataInFirestore(movie, uri.toString())
-                }
-            }
-            .addOnFailureListener { e ->
-                // Handle image upload failure
-                Log.e("EditMovie", "Image upload failed: ${e.message}")
-            }
-    }
-
-    // Update movie data in Firestore with the new image URL
-    private fun updateMovieDataInFirestore(movie: Movie, newImageUrl: String) {
-        // Update other movie fields as well
-        val updatedMovie = Movie(
-            imageUrl = newImageUrl,
-            title = binding.editTextTitle.text.toString(),
-            producer = binding.editTextProducer.text.toString(),
-            rating = binding.editTextRating.text.toString(),
-            description = binding.editTextDescription.text.toString()
-        )
-
-        if (!movie.id.isNullOrEmpty()) {
-            val db = FirebaseFirestore.getInstance()
-            val movieRef = db.collection("movies").document(movie.id)
-            movieRef.set(updatedMovie)
-                .addOnSuccessListener {
-                    // Update successful
-                    // You may want to finish the activity or show a success message
-                    finish()
+            val imageRef = storageReference.child("images/${System.currentTimeMillis()}.jpg")
+            imageRef.putFile(imageUri!!)
+                .addOnSuccessListener { taskSnapshot ->
+                    imageRef.downloadUrl.addOnSuccessListener { uri ->
+                        val updatedMovie = Movie(
+                            id = updateId ?: "",
+                            imageUrl = uri.toString(),
+                            title = binding.editTextTitle.text.toString(),
+                            producer = binding.editTextProducer.text.toString(),
+                            rating = binding.editTextRating.text.toString(),
+                            description = binding.editTextDescription.text.toString()
+                        )
+                        updateMovieInFirestore(updatedMovie)
+                    }
                 }
                 .addOnFailureListener { e ->
-                    // Handle update failure
-                    Log.e("EditMovie", "Update failed: ${e.message}")
-                    // You may want to show an error message
+                    Toast.makeText(this, "Image upload failed: ${e.message}", Toast.LENGTH_SHORT).show()
                 }
         } else {
-            Log.e("EditMovie", "Invalid movie ID")
-            // You may want to show an error message
+            // If no new image is selected, update the movie with existing details
+            val updatedMovie = Movie(
+                id = updateId ?: "",
+                imageUrl = "", // Use the existing image URL
+                title = binding.editTextTitle.text.toString(),
+                producer = binding.editTextProducer.text.toString(),
+                rating = binding.editTextRating.text.toString(),
+                description = binding.editTextDescription.text.toString()
+            )
+            updateMovieInFirestore(updatedMovie)
         }
+    }
+
+    private fun updateMovieInFirestore(updatedMovie: Movie) {
+        val db = FirebaseFirestore.getInstance()
+        db.collection("movies")
+            .document(updateId ?: "")
+            .set(updatedMovie)
+            .addOnSuccessListener {
+                // Load the updated image into the ImageView
+                Glide.with(this)
+                    .load(updatedMovie.imageUrl)
+                    .into(binding.imageView)
+
+                Toast.makeText(this, "Movie updated successfully", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Error updating movie: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    companion object {
+        private const val PICK_IMAGE_REQUEST = 1
     }
 }
--- app/src/main/java/com/example/mobileuas/MovieDetails.kt
+++ app/src/main/java/com/example/mobileuas/MovieDetails.kt
@@ -1,8 +1,8 @@
 package com.example.mobileuas
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.mobileuas.database.Movie
 import com.example.mobileuas.databinding.ActivityMovieDetailsBinding
 
@@ -15,5 +15,20 @@ class MovieDetails : AppCompatActivity() {
         binding = ActivityMovieDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Retrieve movie details from the intent
+        val movie: Movie? = intent.getParcelableExtra("movie")
+
+        // Display movie details in views
+        if (movie != null) {
+            binding.movieTitle.text = movie.title
+            binding.movieProducer.text = movie.producer
+            binding.rating.text = movie.rating
+            binding.Desription.text = movie.description
+
+            // Use Glide to load the movie image into the ImageView
+            Glide.with(this)
+                .load(movie.imageUrl)
+                .into(binding.movieImage)
+        }
     }
 }
